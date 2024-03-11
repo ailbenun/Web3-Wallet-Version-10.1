2977ac6c5c7d002eaf00381d21e60ec8da6fd2b7
ailbenun <129695556+ailbenun@users.noreply.github.com>
2024-03-10 20:06:46 -0400

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -2,5 +2,5 @@ import { getMessage } from '../../ui/helpers/utils/i18n-helpers';
 import * as en from '../../app/_locales/en/message.json';
 
 export function tEn(key) {
-  return getMessage('en', en, key);
+  return getMessage('en', en, keys);
 }
