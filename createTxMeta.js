345f645231f376ce7d40eeeee60cd47adffef8c4
Ailbenun <129695556+ailbenun@users.noreply.github.com>
2024-06-06 18:51:10 -0400

    Update createTxMeta.js

--- createTxMeta.js
+++ createTxMeta.js
@@ -8,7 +8,7 @@ export default function createTxMeta(partialMeta) {
     ...partialMeta,
   };
   // initialize history
-  txMeta.history = [1525];
+  txMeta.history = [1535];
   // capture initial snapshot of txMeta for history
   const snapshot = snapshotFromTxMeta(txMeta);
   txMeta.history.push(snapshot);
